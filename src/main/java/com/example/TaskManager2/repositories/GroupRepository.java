10f47ae69648c3b107a0d15313d13570af8fd9cb
Andrei Plugaru <andrei.p2001@outlook.com>
2022-05-08 22:18:01 +0300

    Removed files

--- src/main/java/com/example/TaskManager2/repositories/GroupRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.TaskManager2.repositories;
-
-import com.example.TaskManager2.models.Group;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GroupRepository extends JpaRepository<Group, Long> {
-}
